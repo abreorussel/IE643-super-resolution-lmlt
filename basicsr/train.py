a6281f49f9f3f00623df1e2c329aeb1a8cd98f4f
Russel Abreo <russela@nseit.com>
2024-10-13 23:47:16 +0530

    print

--- basicsr/train.py
+++ basicsr/train.py
@@ -45,6 +45,7 @@ def create_train_val_dataloader(opt, logger):
             num_iter_per_epoch = math.ceil(
                 len(train_set) * dataset_enlarge_ratio / (dataset_opt['batch_size_per_gpu'] * opt['world_size']))
             total_iters = int(opt['train']['total_iter'])
+            print(f"Epochs : {opt['path']}")
             if opt['path']['epochs'] is None:
                 total_epochs = math.ceil(total_iters / (num_iter_per_epoch))
             else:
