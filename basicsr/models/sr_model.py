c3fa4dca40e7a1bbe3d4747149e2218613e7bfc2
Russel Abreo <russela@nseit.com>
2024-10-22 18:08:54 +0530

    changes

--- basicsr/models/sr_model.py
+++ basicsr/models/sr_model.py
@@ -37,12 +37,12 @@ class SRModel(BaseModel):
 
                 print("Freezing  the Attention Blocks ..........")
                 for i in range(6):  # Adjust this number based on how many blocks you want to freeze
-                    for param in self.net.feats[i].parameters():
+                    for param in self.net_g.feats[i].parameters():
                         param.requires_grad = False
-                for i in range(6, len(self.net.feats)):  # Unfreeze the last 2 blocks
-                    for param in self.net.feats[i].parameters():
+                for i in range(6, len(self.net_g.feats)):  # Unfreeze the last 2 blocks
+                    for param in self.net_g.feats[i].parameters():
                         param.requires_grad = True
-                for param in self.net.to_img.parameters():
+                for param in self.net_g.to_img.parameters():
                     param.requires_grad = True
                 
 
